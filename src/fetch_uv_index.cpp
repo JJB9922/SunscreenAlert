a6783e0f6647c2885d4d0a496aaa381ead30677b
JJB9922 <jjb9922@protonmail.com>
2024-04-24 19:06:25 +0100

    idk

--- src/fetch_uv_index.cpp
+++ src/fetch_uv_index.cpp
@@ -1,46 +1,53 @@
 #include "fetch_uv_index.h"
+#include <esp_http_client.h>
+
 
 //Weather api endpoint incl. api key in secrets
 
-esp_err_t http_event_handler(esp_http_client_event_t *evt)
-{
+esp_err_t http_event_handler(esp_http_client_event_t *evt) {
     switch(evt->event_id) {
+        case HTTP_EVENT_ERROR:
+            printf("HTTP_EVENT_ERROR\n");
+            break;
         case HTTP_EVENT_ON_CONNECTED:
-            ESP_LOGI("HTTP", "HTTP_EVENT_ON_CONNECTED");
+            printf("HTTP_EVENT_ON_CONNECTED\n");
+            break;
+        case HTTP_EVENT_HEADER_SENT:
+            printf("HTTP_EVENT_HEADER_SENT\n");
+            break;
+        case HTTP_EVENT_ON_HEADER:
+            printf("HTTP_EVENT_ON_HEADER, key=%s, value=%s\n", evt->header_key, evt->header_value);
             break;
         case HTTP_EVENT_ON_DATA:
-            ESP_LOGI("HTTP", "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
-            if (!esp_http_client_is_chunked_response(evt->client)) {
-                printf("%.*s", evt->data_len, (char*)evt->data);
-            }
+            printf("HTTP_EVENT_ON_DATA, len=%d\n", evt->data_len);
             break;
         case HTTP_EVENT_ON_FINISH:
-            ESP_LOGI("HTTP", "HTTP_EVENT_ON_FINISH");
+            printf("HTTP_EVENT_ON_FINISH\n");
             break;
-        case HTTP_EVENT_ERROR:
-            ESP_LOGE("HTTP", "HTTP_EVENT_ERROR");
+        case HTTP_EVENT_DISCONNECTED:
+            printf("HTTP_EVENT_DISCONNECTED\n");
             break;
         default:
+            printf("SOMETHING ELSE\n");
             break;
     }
     return ESP_OK;
 }
 
-void fetch_uv_index()
-{
+void fetch_uv_index() {
+
     esp_http_client_config_t config = {
         .url = WEATHER_API_ENDPOINT,
         .event_handler = http_event_handler,
+        .skip_cert_common_name_check = false // Set to false to not skip common name check
     };
-
     esp_http_client_handle_t client = esp_http_client_init(&config);
     esp_err_t err = esp_http_client_perform(client);
-    
+
     if (err == ESP_OK) {
-        ESP_LOGI("HTTP", "Connected to API successfully");
+        printf("OKIE DOKIE");
     } else {
-        ESP_LOGE("HTTP", "HTTP request failed: %s", esp_err_to_name(err));
+        printf("HTTP GET request failed: %s\n", esp_err_to_name(err));
     }
-    
     esp_http_client_cleanup(client);
 }
