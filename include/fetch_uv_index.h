8f22556f680fb5e2fe792612d0d50009930622c7
JJB9922 <jjb9922@protonmail.com>
2024-04-25 19:59:54 +0100

    fixeded the API Request

--- include/fetch_uv_index.h
+++ include/fetch_uv_index.h
@@ -2,6 +2,7 @@
 #define FETCH_UV_INDEX_H
 
 #include "secrets.h"
+
 #include "esp_system.h"
 #include "esp_event.h"
 #include "esp_http_client.h"
--- src/fetch_uv_index.cpp
+++ src/fetch_uv_index.cpp
@@ -1,53 +1,56 @@
 #include "fetch_uv_index.h"
-#include <esp_http_client.h>
 
-
-//Weather api endpoint incl. api key in secrets
+// Weather api endpoint incl. api key in secrets
 
 esp_err_t http_event_handler(esp_http_client_event_t *evt) {
     switch(evt->event_id) {
         case HTTP_EVENT_ERROR:
-            printf("HTTP_EVENT_ERROR\n");
+            ESP_LOGE("HTTP_CLIENT", "HTTP_EVENT_ERROR");
             break;
         case HTTP_EVENT_ON_CONNECTED:
-            printf("HTTP_EVENT_ON_CONNECTED\n");
+            ESP_LOGI("HTTP_CLIENT", "HTTP_EVENT_ON_CONNECTED");
             break;
         case HTTP_EVENT_HEADER_SENT:
-            printf("HTTP_EVENT_HEADER_SENT\n");
+            ESP_LOGI("HTTP_CLIENT", "HTTP_EVENT_HEADER_SENT");
             break;
         case HTTP_EVENT_ON_HEADER:
-            printf("HTTP_EVENT_ON_HEADER, key=%s, value=%s\n", evt->header_key, evt->header_value);
+            ESP_LOGI("HTTP_CLIENT", "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
             break;
         case HTTP_EVENT_ON_DATA:
-            printf("HTTP_EVENT_ON_DATA, len=%d\n", evt->data_len);
+            ESP_LOGI("HTTP_CLIENT", "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
             break;
         case HTTP_EVENT_ON_FINISH:
-            printf("HTTP_EVENT_ON_FINISH\n");
+            ESP_LOGI("HTTP_CLIENT", "HTTP_EVENT_ON_FINISH");
             break;
         case HTTP_EVENT_DISCONNECTED:
-            printf("HTTP_EVENT_DISCONNECTED\n");
+            ESP_LOGI("HTTP_CLIENT", "HTTP_EVENT_DISCONNECTED");
             break;
         default:
-            printf("SOMETHING ELSE\n");
+            ESP_LOGE("HTTP_CLIENT", "CERTIFIED BAD TIMES HAPPENED");
             break;
     }
     return ESP_OK;
 }
 
-void fetch_uv_index() {
-
+void fetch_uv_index()
+{   
     esp_http_client_config_t config = {
         .url = WEATHER_API_ENDPOINT,
+        .auth_type = HTTP_AUTH_TYPE_NONE,
         .event_handler = http_event_handler,
-        .skip_cert_common_name_check = false // Set to false to not skip common name check
     };
+
     esp_http_client_handle_t client = esp_http_client_init(&config);
     esp_err_t err = esp_http_client_perform(client);
 
-    if (err == ESP_OK) {
-        printf("OKIE DOKIE");
-    } else {
-        printf("HTTP GET request failed: %s\n", esp_err_to_name(err));
+    if (err == ESP_OK)
+    {
+        printf("Connected!\n");
+        // Next step - parse all this 
+    }
+    else
+    {
+        ESP_LOGE("HTTP_CLIENT", "HTTP GET request failed: %s", esp_err_to_name(err));
     }
     esp_http_client_cleanup(client);
 }
